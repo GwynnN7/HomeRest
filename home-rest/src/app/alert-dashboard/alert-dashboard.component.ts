40d6c7b0fe1db21735972b647816c12b8f39d643
GwynbleiddN7 <mattcheru03@gmail.com>
2025-06-24 12:39:45 +0200

    Icons and package.json

--- home-rest/src/app/alert-dashboard/alert-dashboard.component.ts
+++ home-rest/src/app/alert-dashboard/alert-dashboard.component.ts
@@ -1,19 +1,15 @@
 import {Component, inject, OnInit} from '@angular/core';
 import {FirebaseService} from '../firebase.service';
-import {DeviceViewComponent} from '../device-view/device-view.component';
 import {NgForOf, NgIf} from '@angular/common';
 import {DeviceInfo} from '../device-info';
 import {Router} from '@angular/router';
-import {SensorViewComponent} from '../sensor-view/sensor-view.component';
 import {AlertView} from '../alert-view/alert-view';
 
 @Component({
   selector: 'app-alert-dashboard',
   imports: [
-    DeviceViewComponent,
     NgForOf,
     NgIf,
-    SensorViewComponent,
     AlertView
   ],
   templateUrl: './alert-dashboard.component.html',
